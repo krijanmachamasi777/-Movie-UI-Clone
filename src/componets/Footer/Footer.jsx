147cd91b9c311fb261ec4de02ff29dad4164d36e
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-06-09 21:23:32 +0545

    fotter updated

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -9,25 +9,25 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className="footer-icons">
-        <img src={facebook_icon} alt="" />
-        <img src={instagram_icon} alt="" />
-        <img src={twitter_icon} alt="" />
-        <img src={youtube_icon} alt="" />
+        <a href="https://www.facebook.com/NetflixAsia/?brand_redir=475822799216240" target='_blank'> <img src={facebook_icon} alt="" /></a>
+        <a href="https://www.instagram.com/netflix/" target='_blank' ><img src={instagram_icon} alt="" /></a>
+        <a href="https://x.com/netflix" target='_blank'> <img src={twitter_icon} alt="" /></a>
+        <a href="https://www.youtube.com/channel/UCWOA1ZGywLbqmigxE4Qlvuw" target='_blank'><img src={youtube_icon} alt="" /></a>
 
       </div>
       <ul>
         <li>Audio Description</li>
-      <li>Help Center</li>
-      <li>Gift Cards</li>
-      <li>Media Center</li>
-      <li>Investor Relations</li>
-      <li>Jobs</li>
-      <li>Terms of Use</li>
-      <li>Privacy</li>
-      <li>Legal Notices</li>
-      <li>Cookies Preferences</li>
-      <li>Corporate Information</li>
-      <li>Contact Us</li>
+        <li>Help Center</li>
+        <li>Gift Cards</li>
+        <li>Media Center</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookies Preferences</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
       </ul>
       <p className='copyright-text'>@ 1997-2023 Netflix , Inc.</p>
     </div>
