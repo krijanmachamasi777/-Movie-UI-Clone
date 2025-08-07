b81a74eee660c1d55a1c9e090e36287ed352bda0
krijanmachamasi777 <krijanmachamasi37@gmail.com>
2025-08-07 23:01:17 +0545

    L is changed to l

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './login.css';
 import logo from '../../assets/logo.png';
 import google from '../../assets/google.png';
 import netflix_spinner from '../../assets/netflix_spinner.gif';
